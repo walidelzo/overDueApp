c94d7b3eb5fa9bad8d8658989bfb3bcea26436f7
walidelzo <walidelzo@gmail.com>
2018-09-01 14:24:04 +0200

    Initial Commit

---
+++ overDueApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  overDueApp
+//
+//  Created by walidelzo on 9/1/18.
+//  Copyright © 2018 walidelzo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ overDueApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  overDueApp
+//
+//  Created by walidelzo on 9/1/18.
+//  Copyright © 2018 walidelzo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
