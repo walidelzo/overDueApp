f359868d243d275fe166b18e27f690272968b29a
walidelzo <walidelzo@gmail.com>
2018-09-01 22:08:19 +0200

    first commit

---
+++ overDueApp/DetailTaskViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailTaskViewController.h
+//  overDueApp
+//
+//  Created by walidelzo on 9/1/18.
+//  Copyright © 2018 walidelzo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailTaskViewController : UIViewController
+
+@end
---
+++ overDueApp/DetailTaskViewController.m
@@ -0,0 +1,37 @@
+//
+//  DetailTaskViewController.m
+//  overDueApp
+//
+//  Created by walidelzo on 9/1/18.
+//  Copyright © 2018 walidelzo. All rights reserved.
+//
+
+#import "DetailTaskViewController.h"
+
+@interface DetailTaskViewController ()
+
+@end
+
+@implementation DetailTaskViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
